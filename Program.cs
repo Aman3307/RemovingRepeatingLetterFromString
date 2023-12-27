09cb79e786224b0016c888cddc0e2937bbeedb57
Aman <Aman.Kumar@interactive12.com>
2023-12-27 15:39:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string inputString = Console.ReadLine();
+
+        int length = inputString.Length;
+
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = i + 1; j < length;)
+            {
+                char currentChar = inputString[i];
+                char iteratingChar = inputString[j];
+
+                if (currentChar == iteratingChar)
+                {
+                    inputString = inputString.Remove(j, 1);
+                    length = length - 1;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+        }
+
+        Console.WriteLine("The String after removing repetitive characters is: " + inputString);
+        Console.ReadKey();
+    }
+}
